d09ff8df1d26918052cf6f688221624d6b0ad916
ShahiCrafts <dev.shahi.apps@gmail.com>
2025-06-01 16:04:40 +0545

    Route Management

--- lib/core/config/routes.dart
+++ lib/core/config/routes.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:ongo_desk/presentation/screens/auth/login_screen.dart';
 import 'package:ongo_desk/presentation/screens/auth/signup_screen.dart';
+// import 'package:ongo_desk/presentation/screens/auth/signup_screen.dart';
 import 'package:ongo_desk/presentation/screens/dashboard/dashboard_screen.dart';
-import 'package:ongo_desk/presentation/screens/splash/splash_screen.dart';
+// import 'package:ongo_desk/presentation/screens/splash/splash_screen.dart';
 
 class AppRoutes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => SplashScreen());
+        return MaterialPageRoute(builder: (_) => DashboardScreen());
 
       case '/login':
         return MaterialPageRoute(builder: (_) => const LoginScreen());
---
+++ lib/presentation/widgets/switcher_bottom_sheet.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class ProfileOptionsBottomSheet extends StatelessWidget {
+  final String accountName;
+
+  const ProfileOptionsBottomSheet({
+    super.key,
+    this.accountName = 'Saugat Shahi',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+        height: 250,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Handle bar
+            Center(
+              child: Container(
+                width: 60,
+                height: 4,
+                margin: const EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+
+            // Title
+            const Text(
+              'Accounts',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+            const Divider(),
+
+            // Main Account Row
+            ListTile(
+              leading: const CircleAvatar(
+                radius: 14,
+                backgroundColor: Color(0xFFFF5C00),
+                child: Icon(Icons.person, color: Colors.white, size: 16,),
+              ),
+              title: Text(accountName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+              trailing: IconButton(
+                icon: const Icon(Icons.logout, size: 20,),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              onTap: () {},
+            ),
+
+            // Anonymous Browsing
+            ListTile(
+              leading: const CircleAvatar(
+                radius: 14,
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.health_and_safety,  color: Colors.white, size: 16,),
+              ),
+              title: const Text('Anonymous Browsing'),
+              onTap: () {
+                // Anonymous action
+                Navigator.pop(context);
+              },
+            ),
+
+            // Add Account
+            ListTile(
+              leading: const CircleAvatar(
+                radius: 14,
+                backgroundColor: Colors.black87,
+                child: Icon(Icons.add, color: Colors.white, size: 16,),
+              ),
+              title: const Text('Add another account'),
+              onTap: () {
+                // Add account action
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
