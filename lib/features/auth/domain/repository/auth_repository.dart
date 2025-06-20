6ccf92409c7de663ea2aa41778c92cf8b7f06363
ShahiCrafts <dev.shahi.apps@gmail.com>
2025-06-20 15:44:04 +0545

    Implement authentication repository with local data source and error handling

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:ongo_desk/core/error/failure.dart';
+import 'package:ongo_desk/features/auth/domain/entity/user_entity.dart';
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure, void>> createAccount(UserEntity user);
+  Future<Either<Failure, String>> loginToAccount(String email, String password);
+}
