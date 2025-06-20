0299e0093c19960610fa770f3d2ddcc33f4d27b6
ShahiCrafts <dev.shahi.apps@gmail.com>
2025-06-20 15:43:40 +0545

    Add OTP verification image and implement app routing
    - Added a new image for OTP verification at assets/images/otp-verify.png.
    - Created a new routes.dart file to manage app navigation.
    - Defined routes for splash screen, login, signup, verification code, role selection, password entry, and dashboard.
    - Integrated BlocProvider for state management in login and signup views.
    - Updated pubspec.lock with new dependencies and version updates.

---
+++ lib/app/config/routes.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ongo_desk/app/get_it/service_locator.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view/details_entry_view.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view/email_entry_view.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view/login_view.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view/password_entry_view.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view/verification_code_view.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view_model/login_view_model/login_view_model.dart';
+import 'package:ongo_desk/features/auth/presentation/auth/view_model/signup_view_model/signup_view_model.dart';
+import 'package:ongo_desk/presentation/screens/dashboard/dashboard_screen.dart';
+import 'package:ongo_desk/presentation/screens/splash/splash_screen.dart';
+
+class AppRoutes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => SplashScreen());
+
+      case '/login':
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider.value(
+                value: serviceLocator<LoginViewModel>(),
+                child: LoginView(),
+              ),
+        );
+
+      case '/signup':
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider.value(
+                value: serviceLocator<SignupViewModel>(),
+                child: EmailEntryView(),
+              ),
+        );
+
+      case '/verify-code':
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider.value(
+                value: serviceLocator<SignupViewModel>(),
+                child: VerificationCodeView(),
+              ),
+        );
+
+      case '/role':
+        return MaterialPageRoute(builder: (_) => BlocProvider.value(
+                value: serviceLocator<SignupViewModel>(),
+                child: DetailsEntryView(),
+              ),);
+
+      case '/password-entry':
+        return MaterialPageRoute(builder: (_) => BlocProvider.value(
+                value: serviceLocator<SignupViewModel>(),
+                child: PasswordEntryView(),
+              ),);
+
+      case '/dashboard':
+        return MaterialPageRoute(builder: (_) => DashboardScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder:
+              (_) =>
+                  const Scaffold(body: Center(child: Text('Page not found!'))),
+        );
+    }
+  }
+}
