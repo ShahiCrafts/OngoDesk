7ba7a2a4a2e1297e257a5411e4d422494a634b2d
ShahiCrafts <dev.shahi.apps@gmail.com>
2025-07-03 22:11:23 +0545

    feat: Add navigation events for signup and dashboard in LoginViewModel

--- lib/features/auth/presentation/view_model/login_view_model/login_event.dart
+++ lib/features/auth/presentation/view_model/login_view_model/login_event.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 abstract class LoginEvent extends Equatable {
   const LoginEvent();
@@ -29,21 +30,32 @@ class TogglePasswordVisibility extends LoginEvent {}
 
 class RememberMeToggled extends LoginEvent {
   final bool value;
-  
+
   const RememberMeToggled(this.value);
 
-  @override 
+  @override
   List<Object?> get props => [value];
+}
+
+class NavigateToSignupEvent extends LoginEvent {
+  final BuildContext context;
+  const NavigateToSignupEvent({required this.context});
+}
+
+class NavigateToDashboardEvent extends LoginEvent {
+  final BuildContext context;
+  const NavigateToDashboardEvent({required this.context});
 }
 
 class LoginSubmitted extends LoginEvent {
   final String email;
   final String password;
+  final BuildContext context;
 
-  const LoginSubmitted(this.email, this.password);
+  const LoginSubmitted(this.email, this.password, this.context);
 
   @override
-  List<Object?> get props => [email, password];
+  List<Object?> get props => [email, password, context];
 }
 
 class ResetFormStatus extends LoginEvent {}
--- lib/features/auth/presentation/view_model/login_view_model/login_view_model.dart
+++ lib/features/auth/presentation/view_model/login_view_model/login_view_model.dart
@@ -1,21 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ongo_desk/core/common/snackbar/my_snackbar.dart';
 import 'package:ongo_desk/features/auth/domain/usecase/auth_login_usecase.dart';
 import 'package:ongo_desk/features/auth/presentation/view_model/login_view_model/login_event.dart';
 import 'package:ongo_desk/features/auth/presentation/view_model/login_view_model/login_state.dart';
 
 class LoginViewModel extends Bloc<LoginEvent, LoginState> {
-
   final AuthLoginUsecase _authLoginUsecase;
 
-  LoginViewModel(
-    this._authLoginUsecase
-  ) : super(LoginState()) {
+  LoginViewModel(this._authLoginUsecase) : super(LoginState()) {
     on<LoginEmailChanged>(_onEmailChanged);
     on<LoginPasswordChanged>(_onPasswordChanged);
     on<LoginSubmitted>(_onLoginSubmitted);
     on<TogglePasswordVisibility>(_onTogglePassword);
     on<RememberMeToggled>(_onToggleRememberMe);
     on<ResetFormStatus>(_onResetFormStatus);
+    on<NavigateToSignupEvent>(_onNavigateToSignup);
+    on<NavigateToDashboardEvent>(_onNavigateToDashboard);
   }
 
   void _onEmailChanged(LoginEmailChanged event, Emitter<LoginState> emit) {
@@ -24,13 +25,19 @@ class LoginViewModel extends Bloc<LoginEvent, LoginState> {
     emit(state.copyWith(email: email));
   }
 
-  void _onPasswordChanged(LoginPasswordChanged event, Emitter<LoginState> emit) {
+  void _onPasswordChanged(
+    LoginPasswordChanged event,
+    Emitter<LoginState> emit,
+  ) {
     final password = event.password;
 
     emit(state.copyWith(password: password));
   }
 
-  void _onTogglePassword(TogglePasswordVisibility event, Emitter<LoginState> emit) {
+  void _onTogglePassword(
+    TogglePasswordVisibility event,
+    Emitter<LoginState> emit,
+  ) {
     emit(state.copyWith(obscurePassword: !state.obscurePassword));
   }
 
@@ -40,26 +47,64 @@ class LoginViewModel extends Bloc<LoginEvent, LoginState> {
     emit(state.copyWith(rememberMe: rememberMe));
   }
 
+  void _onNavigateToSignup(
+    NavigateToSignupEvent event,
+    Emitter<LoginState> emit,
+  ) {
+    if (event.context.mounted) {
+      Navigator.pushNamed(event.context, '/signup');
+    }
+  }
+
+  void _onNavigateToDashboard(
+    NavigateToDashboardEvent event,
+    Emitter<LoginState> emit,
+  ) {
+    if (event.context.mounted) {
+      Navigator.pushNamed(event.context, '/dashboard');
+    }
+  }
+
   void _onLoginSubmitted(LoginSubmitted event, Emitter<LoginState> emit) async {
     final email = state.email;
     final password = state.password;
 
-    emit(state.copyWith(formStatus: FormStatus.submitting, message: 'Submission Under Process'));
+    emit(
+      state.copyWith(
+        formStatus: FormStatus.submitting,
+        message: 'Submission Under Process',
+      ),
+    );
 
     await Future.delayed(Duration(seconds: 2));
 
     final result = await _authLoginUsecase(
-      LoginParams(email: email, password: password)
-    ); 
+      LoginParams(email: email, password: password),
+    );
 
     result.fold(
-      (left)=> {
-        emit(state.copyWith(formStatus: FormStatus.failure, message: 'Login Failed!'))
+      (left) {
+        emit(
+          state.copyWith(
+            formStatus: FormStatus.failure,
+            message: 'Login Failed!',
+          ),
+        );
+        showMySnackBar(
+          context: event.context,
+          message: 'Invalid Credentials, Please try again.',
+        );
       },
 
-      (right) => {
-        emit(state.copyWith(formStatus: FormStatus.success, message: 'Login Successful!'))
-      }
+      (right) {
+        emit(
+          state.copyWith(
+            formStatus: FormStatus.success,
+            message: 'Login Successful!',
+          ),
+        );
+        add(NavigateToDashboardEvent(context: event.context));
+      },
     );
   }
 
